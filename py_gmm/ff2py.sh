54b8d6dcad1ee0e7ce8309b21a6e20aecd92d950
Marek Nečada <marek.necada@aalto.fi>
2015-12-28 16:23:41 +0200

    Wrap Legendreovy funkce

--- py_gmm/ff2py.sh
+++ py_gmm/ff2py.sh
@@ -1,2 +1,3 @@
 f2py3 -m gmm -h gmm.pyf kinds.f90 datatypes.f90 operators.f90 shared_data.f90 basicsubs.f90 gmmsubs.f90 sing_part.f90 vec_trans.f90 local_field.f90 linear_solver.f90 
-f2py3 -c -lopenblas --build-dir ./tmp --f90exec=/usr/bin/gfortran --fcompiler=gnu95 --f90flags='-O3 -fpic -m64' gmm.pyf kinds.f90 datatypes.f90 operators.f90 shared_data.f90 basicsubs.f90 gmmsubs.f90 sing_part.f90 vec_trans.f90 local_field.f90 linear_solver.f90
+f2py3 -c -lopenblas --build-dir ./tmp --f90exec=/usr/bin/gfortran --fcompiler=gnu95 --f90flags='-O3 -fpic -m64' gmm.pyf kinds.f90 datatypes.f90 operators.f90 shared_data.f90 basicsubs.f90 gmmsubs.f90 sing_part.f90 vec_trans.f90 local_field.f90 linear_solver.f90 && cp -r * ~/.local/lib/python3.4/site-packages/py_gmm-0.1-py3.4.egg/py_gmm/
+
